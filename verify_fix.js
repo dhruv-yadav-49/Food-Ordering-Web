96110c06e48a441168b745fcfb8f1e0dd15cadc4
dhruv-yadav-49 <dhruvofficial73@gmail.com>
2026-02-10 23:19:14 +0530

    Done

--- verify_fix.js
+++ verify_fix.js
@@ -4,6 +4,7 @@ try {
     require('./src/service/userService.js');
     require('./src/controllers/authController.js');
     require('./src/index.js');
+    require('./src/routes/authRoutes.js')
     console.log("All modules required successfully");
 } catch (error) {
     console.error("Error requiring modules:", error);
